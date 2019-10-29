79189cf4dcb00013874d7fd079fd492912f271a5
pawelrudnicki <kontakt.prudnicki@gmail.com>
2019-10-29 21:12:02 +0100

    dotnet infrastructure, core and api

---
+++ src/MedicinePlanner.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedicinePlanner.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MedicinePlanner.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedicinePlanner.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
