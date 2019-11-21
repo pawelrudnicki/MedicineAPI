4f4a0c1d890b386e8d723bcb2a3d9f7c0255a586
pawelrudnicki <kontakt.prudnicki@gmail.com>
2019-11-21 21:08:21 +0100

    patient class

---
+++ src/MedicinePlanner.Core/Domain/Patient.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+
+namespace MedicinePlanner.Core.Domain
+{
+    public class Patient
+    {
+        private static List<string> _bloodTypes = new List<string>
+        {
+            "0 rh-", "0 rh+", "a rh-", "a rh+", "b rh-", "b rh+", "ab rh-", "ab rh+"
+        };
+        private ISet<Medicine> _medicines = new HashSet<Medicine>();
+        public Guid Id { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public String Name { get; protected set; }
+        public int Age { get; protected set; }
+        public String BloodType { get; protected set; }
+        public Double Weight { get; protected set; }
+        public Double Height { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+        public IEnumerable<Medicine> Medicines  //todo - sprawdzic czy nie trzeba stworzyc osobnej klasy
+        {
+            get { return _medicines; }
+            set { _medicines = new HashSet<Medicine>(value); }
+        }
+
+        protected Patient()
+        {
+        }
+
+        public Patient(User user)
+        {
+            UserId = user.Id;
+            Name = user.Name;
+        }
+
+        public void SetAge(int age)
+        {
+            if (age <= 0)
+            {
+                throw new Exception("Age can not be less than 1.");
+            }
+
+            if (Age == age)
+            {
+                return;
+            }
+
+            Age = age;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetBloodType(string bloodType)
+        {
+            if (string.IsNullOrWhiteSpace(bloodType))
+            {
+                throw new Exception("Blood type can not be empty.");
+            }
+
+            bloodType = bloodType.ToLowerInvariant();
+
+            if (!_bloodTypes.Contains(bloodType))
+            {
+                throw new Exception($"Patient can not have a blood type '{bloodType}'");
+            }
+
+            BloodType = bloodType;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetWeight(Double weight)
+        {
+            if (weight <= 0)
+            {
+                throw new Exception("Weight can not be less than 1.");
+            }
+
+            if (Weight == weight)
+            {
+                return;
+            }
+
+            Weight = weight;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetHeight(Double height)
+        {
+            if (height <= 0)
+            {
+                throw new Exception("Height can not be less than 1.");
+            }
+
+            if (Height == height)
+            {
+                return;
+            }
+
+            Height = height;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
